82abff42e957db4c36266f1be14e852b55a4bdff
BFT\a.denezhkin <a.denezhkin@bftcom.com>
2021-11-13 17:38:38 +0400

    Added Qualified

--- src/main/java/ru/aden/springcourse/models/MusicPlayer.java
+++ src/main/java/ru/aden/springcourse/models/MusicPlayer.java
@@ -1,23 +1,25 @@
 package ru.aden.springcourse.models;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import ru.aden.springcourse.models.impls.ClassicalMusic;
 import ru.aden.springcourse.models.impls.RockMusic;
 
 @Component
 public class MusicPlayer {
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
+    private Music music1;
+    private Music music2;
 
-   @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
+    @Autowired
+    public MusicPlayer(@Qualifier("classicalMusic") Music music1,
+                       @Qualifier("rockMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
     public String playMusic() {
-        return "Playing now: " + classicalMusic.getSong();
+        return "Playing now: " + music1.getSong() + " , " + music2.getSong();
 
     }
 }
